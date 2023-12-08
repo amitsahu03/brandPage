1469c8d25f1b012044aced59914c16b1ba491c21
golus-bhaiya <amitsahuald03@gmail.com>
2023-12-08 23:56:39 +0700

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hero from './components/Hero';
+import Navigation from './components/Navigation';
+
+function App(){
+  return <>
+  <Navigation></Navigation>
+  <Hero></Hero>
+  
+  </>
+}
+export default App;
